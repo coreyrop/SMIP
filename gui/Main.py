2f377120babdfb3b8f5cf1f609dcada6472eec20
Josh <jtsutten@buffalo.edu>
2019-02-17 18:54:40 -0500

    Added functionality to quit button

--- gui/Main.py
+++ gui/Main.py
@@ -14,13 +14,17 @@ root.minsize(450, 400)
 ttk.Style().configure('green/black.TLabel', foreground='black', background='white')
 ttk.Style().configure('green/black.TButton', foreground='black', background='white')
 
+
 label = ttk.Label(root, text='Welcome to SMIP. Your Best Friend for Learning MIPS', style='green/black.TLabel')
 label.pack()
 
 button1 = ttk.Button(root, text='Start', style='green/black.TButton')
 button2 = ttk.Button(root, text='Select Lesson', style='green/black.TButton')
 button3 = ttk.Button(root, text='Practice', style='green/black.TButton')
-button4 = ttk.Button(root, text='Exit', style='green/black.TButton')
+button4 = ttk.Button(root, text='Exit', style='green/black.TButton', command=quit);
+
+def quit():
+    root.destroy()
 
 button1.pack()
 button2.pack()
