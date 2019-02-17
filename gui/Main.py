821648653c13ef1a46afe6237d00f9fe7e92ceea
Christian Rowe-Slack <kd2izv@gmail.com>
2019-02-17 18:42:57 -0500

    Cprowesl adds title and Window Size functions
    I made it so that we can see the buttons via the configuration dialogue.  I added a relevant title to the window.  I changed some window sizing options so that the window appears as larger and gives more room for work.  I may be changing the size of the buttons soon.

--- gui/Main.py
+++ gui/Main.py
@@ -2,10 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
+root.title("SMIP: The Student MIPS Instruction Program")
+# Turning off pack propagate to prevent widgets from determining window size.
+# Max and Min window sizes may change.
+root.pack_propagate(0)
+root.maxsize(1000, 1000)
+root.minsize(450, 400)
+
 
 # background="..." doesn't work...
-ttk.Style().configure('green/black.TLabel', foreground='black', background='black')
-ttk.Style().configure('green/black.TButton', foreground='black', background='black')
+ttk.Style().configure('green/black.TLabel', foreground='black', background='white')
+ttk.Style().configure('green/black.TButton', foreground='black', background='white')
 
 label = ttk.Label(root, text='Welcome to SMIP. Your Best Friend for Learning MIPS', style='green/black.TLabel')
 label.pack()
