ccd0019e626b2838e3fff96c06dd77dfb01cc084
Christian Rowe-Slack <kd2izv@gmail.com>
2019-02-17 20:44:35 -0500

    Cprowesl pushes button formatting and font changes.
    I updated the fonts and colors on the main menu.  This is a temporary change but it made the buttons more legible for us to work with.  Warning: I have noticed some attributes of the buttons do not work.  At least, it seems like they have no effect on my end.  Many of tkinters functions seem to be deprecated in some areas.  This will be my push before I complete task SMIP #15: Make GUI Frame.

--- gui/Main.py
+++ gui/Main.py
@@ -1,30 +1,42 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 
 root = tk.Tk()
 root.title("SMIP: The Student MIPS Instruction Program")
+
 # Turning off pack propagate to prevent widgets from determining window size.
 # Max and Min window sizes may change.
 root.pack_propagate(0)
 root.maxsize(1000, 1000)
-root.minsize(450, 400)
+root.minsize(525, 310)
+
+# Set fonts for the menu widgets.
+# print(font.families()) to print available font families.
+menuLabel_font = font.Font(family="Loma", size=22, weight="bold")
+menuButton_font = font.Font(family="Loma", size=20, weight="normal")
 
 
 # background="..." doesn't work...
-ttk.Style().configure('green/black.TLabel', foreground='black', background='white')
-ttk.Style().configure('green/black.TButton', foreground='black', background='white')
+ttk.Style().configure('green/black.TLabel', foreground='black', background='DarkOrange1', font=menuLabel_font)
+ttk.Style().configure('green/black.TButton', foreground='black', background='DarkOrange1', font=menuButton_font,
+                      width=15)
 
+slide = tk.Frame(master=root, bg="dodger blue")
+slide.pack()
 
-label = ttk.Label(root, text='Welcome to SMIP. Your Best Friend for Learning MIPS', style='green/black.TLabel')
+label = ttk.Label(slide, text='\tWelcome to SMIP.\n Your Best Friend for Learning MIPS ', style='green/black.TLabel')
 label.pack()
 
-button1 = ttk.Button(root, text='Start', style='green/black.TButton')
-button2 = ttk.Button(root, text='Select Lesson', style='green/black.TButton')
-button3 = ttk.Button(root, text='Practice', style='green/black.TButton')
-button4 = ttk.Button(root, text='Exit', style='green/black.TButton', command=quit);
+button1 = ttk.Button(slide, text='Start', style='green/black.TButton')
+button2 = ttk.Button(slide, text='Select Lesson', style='green/black.TButton')
+button3 = ttk.Button(slide, text='Practice', style='green/black.TButton')
+button4 = ttk.Button(slide, text='Exit', style='green/black.TButton', command=quit)
+
 
 def quit():
     root.destroy()
+
 
 button1.pack()
 button2.pack()
