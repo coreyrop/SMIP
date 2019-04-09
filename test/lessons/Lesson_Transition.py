8f562e33bcb22a3a76cac001388ef182da1eca08
Animesh <asoni@buffalo.edu>
2019-04-09 00:20:16 -0400

    Added some unit tests for lesson transition

--- test/lessons/Lesson_Transition.py
+++ test/lessons/Lesson_Transition.py
@@ -1,10 +1,50 @@
 import unittest as ut
-from lessons.Lesson_Transition import get_next_lesson, get_previous_lesson, set_current_lesson_index, lessons, get_current_lesson
+from lessons.Lesson_Transition import lesson_index, get_next_lesson, get_previous_lesson, set_current_lesson_index, lessons, get_current_lesson
 
-class LessonTransition(ut.TestCase):
+
+
+class TestGetCurrLesson1(ut.TestCase):
+    def runTest(self):
+        set_current_lesson_index(0)
+        lesson = get_current_lesson()
+        self.assertEqual(lesson, lessons[0])
+
+class TestGetCurrLesson2(ut.TestCase):
     def runTest(self):
         set_current_lesson_index(len(lessons)-1)
         lesson = get_current_lesson()
-        next = get_next_lesson()
+        self.assertEqual(lesson, lessons[-1])
 
-        self.assertEquals(lesson, next)
+class TestGetNextLesson1(ut.TestCase):
+    def runTest(self):
+        set_current_lesson_index(0)
+        next = get_next_lesson()
+        set_current_lesson_index(1)
+        lesson = get_current_lesson()
+        self.assertEqual(lesson, next)
+
+class TestGetNextLesson2(ut.TestCase):
+    def runTest(self):
+        set_current_lesson_index(len(lessons)-1)
+        next = get_next_lesson()
+        lesson = get_current_lesson()
+        self.assertEqual(lesson, next)
+
+class TestGetPrevLesson1(ut.TestCase):
+    def runTest(self):
+        set_current_lesson_index(0)
+        prev = get_previous_lesson()
+        lesson = get_current_lesson()
+        self.assertEqual(lesson, prev)
+
+class TestGetPrevLesson2(ut.TestCase):
+    def runTest(self):
+        set_current_lesson_index(len(lessons)-1)
+        prev = get_previous_lesson()
+        set_current_lesson_index(len(lessons)-2)
+        lesson = get_current_lesson()
+        self.assertEqual(lesson, prev)
+
+
+if __name__ == '__main__':
+    ut.main()
