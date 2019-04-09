d519ae01f673e9f27884c4f36dd228efc1410910
Corey Ropell <coreyrop@buffalo.edu>
2019-04-09 13:09:52 -0400

    Fixed issue in Drawer/TransitionMenuLessonTestCase
    Previous implementation was not properly destroying the previous frames in transfer_to() call. Modified code so that the proper frames are destroyed, causing test to run properly in Linux

--- test/gui/Drawer.py
+++ test/gui/Drawer.py
@@ -89,7 +89,6 @@ class BasicTransitionTestCase(ut.TestCase):
     def setUp(self):
         self.frame = tk.Tk()
         self.ttk = ttk
-        self.top_frame = tk.Frame()
         draw_menu(self.frame, self.ttk)
 
 
@@ -97,10 +96,10 @@ class TransitionMenuLessonTestCase(BasicTransitionTestCase):
     def runTest(self):
         print(self.frame.winfo_width())
         assert self.frame.winfo_width() <= 700, 'Main Menu width check failed.'
-        transfer_to(lambda: draw_lesson(self.frame, self.ttk, get_current_lesson()), self.top_frame)
+        transfer_to(lambda: draw_lesson(self.frame, self.ttk, get_current_lesson()), *self.frame.children.values())
         assert self.frame.winfo_width() <= 875, 'Lesson width check failed.'
         assert self.frame.winfo_children() is not [], 'Drawn Lesson failed with no children.'
-        transfer_to(lambda: draw_menu(self.frame, self.ttk), self.top_frame)
+        transfer_to(lambda: draw_menu(self.frame, self.ttk), *self.frame.children.values())
         assert self.frame.winfo_width() <= 700, 'Main Menu width failed after transfer from Lesson.'
         return print('Transition Menu Lesson Test: Pass.')
 
